4ced2275907073b8a3e5ab29effd3e5d222dc464
Tae.Garam <drasla@naver.com>
2025-12-12 13:07:38 +0900

    기본 공사 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import GlobalStyle from "./styles/GlobalStyle.tsx";
+import { RouterProvider } from "react-router";
+import router from "./router/router.tsx";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    height: 100dvh;
+    display: flex;
+    justify-content: center;
+`;
+
 function App() {
-    return <></>
+    return (
+        <Wrapper>
+            <GlobalStyle />
+            <RouterProvider router={router} />
+        </Wrapper>
+    );
 }
 
 export default App;
---
+++ src/router/router.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router";
+import Layout from "../layouts/Layout.tsx";
+import Home from "../pages/Home.tsx";
+import Profile from "../pages/Profile.tsx";
+import Login from "../pages/Login.tsx";
+import CreateAccount from "../pages/CreateAccount.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { path: "", element: <Home /> }, // 경로 : /
+            { path: "profile", element: <Profile /> }, // 경로 : /profile
+        ],
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/createAccount",
+        element: <CreateAccount />,
+    },
+]);
+
+export default router;
