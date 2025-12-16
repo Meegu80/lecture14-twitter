25a80cf6017bfe6e3b317cbc784dcd19c5f95064
Meegu80 <aceman80@naver.com>
2025-12-16 12:57:18 +0900

    파일 삭제 완료 이미지파일 첨부기능 완료 delete버튼 기능 완료 타임라인스타일링 포스팅완료
    logout작성
    ProtectedRoute.tsx 생성
    회원가입 createAccount작성완료

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,8 +1,9 @@
 import type { TweetType } from "./Timeline.tsx";
-import { auth, db } from "../firebase.ts";
+import { auth, db, storage } from "../firebase.ts";
 import styled from "styled-components";
 import { deleteDoc, doc } from "firebase/firestore";
 import { useNavigate } from "react-router";
+import { deleteObject, ref } from "firebase/storage";
 
 type Props = {
     item: TweetType;
@@ -10,10 +11,11 @@ type Props = {
 
 const Wrapper = styled.div`
     display: flex;
-    flex-direction: column;
+    justify-content: space-between;
     padding: 20px;
     border: 1px solid white;
     border-radius: 15px;
+
 `;
 
 const Username = styled.span`
@@ -36,6 +38,15 @@ const DeleteBtn = styled.button`
     border-radius: 5px;
     cursor: pointer;
 `;
+const Column = styled.div`
+    
+`;
+const Photo = styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius: 15px;
+
+`;
 
 function Tweet({ item }: Props) {
     const navigate = useNavigate();
@@ -50,6 +61,11 @@ function Tweet({ item }: Props) {
         try {
             // 재확인 시 진짜 삭제한다면 firestore에서 삭제 처리
             await deleteDoc(doc(db, "tweets", item.id));
+
+            if(item.photo || user?.uid){
+                const photoRef = ref(storage, `tweets/${item.userId}/${item.id}`)
+                await deleteObject(photoRef);
+            }
             // 이후 새로고침
             navigate(0);
         } catch (e) {
@@ -59,10 +75,13 @@ function Tweet({ item }: Props) {
 
     return (
         <Wrapper>
-            <Username>{item.username}</Username>
-            <Text>{item.tweet}</Text>
-            {/* 접속한 사용자와, 글 작성자가 동일할 때에는 삭제 버튼을 출력 */}
-            {user?.uid === item.userId && <DeleteBtn onClick={onDelete}>Delete</DeleteBtn>}
+            <Column>
+                <Username>{item.username}</Username>
+                <Text>{item.tweet}</Text>
+                {/* 접속한 사용자와, 글 작성자가 동일할 때에는 삭제 버튼을 출력 */}
+                {user?.uid === item.userId && <DeleteBtn onClick={onDelete}>Delete</DeleteBtn>}
+            </Column>
+            {item.photo && <Column><Photo src={item.photo} /></Column>}
         </Wrapper>
     );
 }
